f4a4fcebb78cf7ac85522872d78c68423b24e10d
Anak2429 <pulliakshay3@gmail.com>
2025-11-18 13:00:14 +0530

    Added working Delete button and Added relavent comments for the understing the code

--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
+
+// feched all HTML Elements by document.getElementByID() method //
 let Name = document.getElementById("name");
 let submit = document.getElementById("submit");
 let reset = document.getElementById("reset");
 let student_id = document.getElementById("student_id");
 let student_email =document.getElementById("student_email");
 let contact = document.getElementById("Contact_NO");
-
-console.log(name,submit,reset,student_email,student_id,contact);
-
-submit.addEventListener("click",addDetails);
+let StudetForm = document.getElementById("studetnForm");
 const tableBody = document.getElementById("tableBody");
 
-function addDetails() {
-    console.log("SUBMITTED");
+//Adding the EventListener ont he Submit Button and Calling th addDetails() function //
 
+submit.addEventListener("click",addDetails);
+
+// addDetails()  for Adding the Details to the Registerd Student List //
+function addDetails() {
+    // Created the HTML Elements todisplay fetched data from the StudentForm ///
     const addRow = document.createElement("tr");
     const name_th = document.createElement("td");
     const id_th = document.createElement("td");
     const email_th = document.createElement("td");
     const contact_th = document.createElement("td");
     const remove = document.createElement("td");
-    const deleteBTN =document.createComment("button");
+    //Added Delete button for the Delete operation on row ///
+    const deleteBTN =document.createElement("button");
     deleteBTN.textContent = "Delete";
     deleteBTN.className = "px-3 py-1 bg-red-500 text-white rounded-md";
+
+    // Added EventListener to teh Delete button For deleting the row in the display section //
+    deleteBTN.addEventListener("click", ()=>{
+        addRow.remove();
+    })
+
     remove.appendChild(deleteBTN);
 
     name_th.innerHTML = Name.value;
@@ -34,5 +44,6 @@ function addDetails() {
 
     tableBody.appendChild(addRow);
 
-    console.log(name_th);
+
+    
 }
