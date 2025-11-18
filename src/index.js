3f7be3e0c87c402ed52f3e8d341f7e2ed380ae2f
Anak2429 <pulliakshay3@gmail.com>
2025-11-18 17:28:10 +0530

    Addded validation syste for the data entered

--- src/index.js
+++ src/index.js
@@ -6,44 +6,73 @@ let reset = document.getElementById("reset");
 let student_id = document.getElementById("student_id");
 let student_email =document.getElementById("student_email");
 let contact = document.getElementById("Contact_NO");
-let StudetForm = document.getElementById("studetnForm");
+let StudentForm = document.getElementById("studentForm");
 const tableBody = document.getElementById("tableBody");
 
 //Adding the EventListener ont he Submit Button and Calling th addDetails() function //
 
 submit.addEventListener("click",addDetails);
+Name.focus();
 
 // addDetails()  for Adding the Details to the Registerd Student List //
-function addDetails() {
-    // Created the HTML Elements todisplay fetched data from the StudentForm ///
+function addDetails(event) {
+    event.preventDefault();
+// validation for the data fetched //
+    if (Name.value.trim() === "") {
+    alert("Please add Student Name");
+    return;
+    }
+
+    if (student_id.value.trim() === "") {
+        alert("Please add Student ID");
+        return;
+    }
+
+    if (student_email.value.trim() === "") {
+        alert("Please add Student Email");
+        return;
+    }
+
+    if (contact.value.trim() === "") {
+        alert("Please add Contact Number");
+        return;
+    }
+
+
+// Created the HTML Elements todisplay fetched data from the StudentForm ///
     const addRow = document.createElement("tr");
     const name_th = document.createElement("td");
     const id_th = document.createElement("td");
     const email_th = document.createElement("td");
     const contact_th = document.createElement("td");
     const remove = document.createElement("td");
-    //Added Delete button for the Delete operation on row ///
+//Added Delete button for the Delete operation on row ///
     const deleteBTN =document.createElement("button");
     deleteBTN.textContent = "Delete";
     deleteBTN.className = "px-3 py-1 bg-red-500 text-white rounded-md";
 
-    // Added EventListener to teh Delete button For deleting the row in the display section //
+// Added EventListener to teh Delete button For deleting the row in the display section //
     deleteBTN.addEventListener("click", ()=>{
         addRow.remove();
     })
+     
+// added fetched data to the table data to display onthe screen
+    name_th.textContent = Name.value;
+    id_th.textContent = student_id.value;
+    email_th.textContent = student_email.value;
+    contact_th.textContent = contact.value;
+    
+    
 
+// appended the delete button to the table row // 
     remove.appendChild(deleteBTN);
-
-    name_th.innerHTML = Name.value;
-    id_th.innerHTML = student_id.value;
-    email_th.innerHTML = student_email.value;
-    contact_th.innerHTML = contact.value;
     
-
+//appended the table data to th row 
     addRow.append(name_th, id_th, email_th, contact_th,remove);
-
-    tableBody.appendChild(addRow);
-
-
     
+//appended the table row to the table 
+    tableBody.appendChild(addRow);
+    StudentForm.reset();
+    Name.focus();
+
 }
